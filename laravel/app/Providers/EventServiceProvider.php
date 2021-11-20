9efbf118c7862004acbd8eb26903ff2bda7adc4f
Maximilian Baehr <forkapp@maximilianbaehr.com>
2021-11-20 20:50:00 +0100

    Fixed Socialite Bug
    Fixed a bug, where the Server would not start due to a Array to String conversion.

--- laravel/app/Providers/EventServiceProvider.php
+++ laravel/app/Providers/EventServiceProvider.php
@@ -15,11 +15,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-            \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-                \SocialiteProviders\Steam\SteamExtendSocialite::class.'@handle',
-            ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            \SocialiteProviders\Steam\SteamExtendSocialite::class.'@handle',
         ],
     ];
 
